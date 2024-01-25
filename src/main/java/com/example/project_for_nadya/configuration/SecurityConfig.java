ae6ee2dabd7ed87d247239390d8cce9de661a7bf
Oleh MIlkov <Oleg.milkov.99@gmail.com>
2024-01-25 14:21:44 +0200

    add security role

--- src/main/java/com/example/project_for_nadya/configuration/SecurityConfig.java
+++ src/main/java/com/example/project_for_nadya/configuration/SecurityConfig.java
@@ -5,17 +5,21 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig  {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
@@ -34,6 +38,8 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 
 }
 
--- src/main/java/com/example/project_for_nadya/controller/MyRestController.java
+++ src/main/java/com/example/project_for_nadya/controller/MyRestController.java
@@ -3,6 +3,7 @@ package com.example.project_for_nadya.controller;
 import com.example.project_for_nadya.entity.Child;
 import com.example.project_for_nadya.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -25,15 +26,15 @@ public class MyRestController {
     }
 
     @RequestMapping("/showAllDebtor")
-    public  String showAllDebtor(Model model){
+    public String showAllDebtor(Model model) {
         List<Child> allDebtor = reportService.getAllDebtor();
         model.addAttribute("allDebtor", allDebtor);
         return "all-Debtor";
     }
 
 
-
     @RequestMapping("/addNewChild")
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE') OR hasRole('ROLE_HR')")
     public String addNewChild(Model model) {
         Child child = new Child();
         model.addAttribute("child", child);
@@ -46,15 +47,15 @@ public class MyRestController {
         return "redirect:/";
     }
 
-@RequestMapping("/mainPage")
-public String mainPage(){
+    @RequestMapping("/mainPage")
+    public String mainPage() {
         return "redirect:/";
-}
+    }
 
 
     @RequestMapping("/updateInfo")
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
     public String updateChild(@RequestParam("childId") int id, Model model) {
-
         Child child = reportService.getChild(id);
         model.addAttribute("child", child);
         return "child-info";
@@ -62,11 +63,11 @@ public String mainPage(){
 
 
     @RequestMapping("/deleteChild")
-    public String deleteChild(@RequestParam("childId")int id){
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    public String deleteChild(@RequestParam("childId") int id) {
         reportService.deleteChild(id);
         return "redirect:/";
     }
-
 
 
 }
